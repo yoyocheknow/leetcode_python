4154bf1e913c448e956d40b0e00f6f3320909a2a
zhihua <zhangzhihua@daixiaomi.com>
2019-11-20 17:45:18 +0800

    第10题 通配符问题-dp解法

--- 10_Regular_Expression_Matching.py
+++ 10_Regular_Expression_Matching.py
@@ -84,7 +84,45 @@ class Solution(object):
         # 下一个递归进入时就是：text='c',pattern='a*c',这个时候又要作出选择，*选择复制还是选择消失，此时如果进入or的这一个判断，选择消失，那么
         # 进入下个递归时：text='c',pattern='c',matched!
 
+    # DP动态规划解法
+    def isMatch1(self, text, pattern):
+        # 用dp(i,j)来表示text[i:],pattern[j:] matched, text从第i+1个字符开始和pattern的第j+1个字符开始匹配
+        # dp思想就是把问题分而治之，把大的问题拆成小问题解决。对于这个题，要想比较整个串是否匹配，那么就可以拆解成：
+        # A:判断text[0]和 pattern[0]是否匹配以及 B:text[1:]和 pattern[1:]是否匹配这两个子问题解决。我们知道A问题很容易判断，
+        # 只需要这样判断即可： first_match = i < len(text) and pattern[j] in {text[i], '.'}
+        # B问题 还可以继续拆解为 B_A:text[1]和pattern[1]比较,B_B:text[2:]和pattern[2:]比较。这个就是dp的整体思路。
+        # 如果没有 * 这个限制条件的话，那么就可以这么写：ans = first_match and dp(i + 1, j + 1)。然后将这个结果存在memo中
+        # 现在有* 这个条件限制：'*' Matches zero or more of the preceding element. *要么和前一个字符一起消失，要么就复制前一个字符
+        # 那么我们在计算 B问题（求剩余字串是否匹配）时把这个限制加上即可。
+        # '*' 如果复制前一个字符，那么只需要判断前一个字符是否匹配，即first_match 为true，只有前一个字符匹配了，才有分解下一部分字串的必要
+        # 也就是first_match and dp(i + 1, j)
+        # '*' 如果是和前一个字符一起消失，那么pattern的指针j只需要往后移动两个即可，text不需要动，即：ans = dp(i, j + 2)
+
+
+
+        # memo{}用来缓存这个结果
+        memo = {}
+        def dp(i, j):
+            # 如果i,j 已经在memo中，则直接返回这个结果
+            if (i, j) not in memo:
+                # 如果j已经走到了pattern尽头，那么判断i是不是也走到尽头，如果不是，则ans为false不匹配，如果是则匹配
+                if j == len(pattern):
+                    ans = i == len(text)
+                else:
+                    first_match = i < len(text) and pattern[j] in {text[i], '.'}
+
+                    if j + 1 < len(pattern) and pattern[j + 1] == '*':
+                        # pattern剩余的字符中第二个为*，走下面的逻辑
+                        ans = dp(i, j + 2) or first_match and dp(i + 1, j)
+                    else:
+                        # pattern剩余的字符中没有*，走下面的逻辑
+                        ans = first_match and dp(i + 1, j + 1)
+                #将结果缓存在memo中
+                memo[i, j] = ans
+            return memo[i, j]
+
+        return dp(0, 0)
 
 if __name__ == "__main__":
-    r = Solution().isMatch('aac', 'a*c')
+    r = Solution().isMatch1('aac', 'a*c')
     print r
