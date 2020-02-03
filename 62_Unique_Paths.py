98a402fdfbeab0c47a0d13d6ce019112a462ed92
yoyocheknow <911dangerous@gmail.com>
2020-02-03 11:31:10 +0800

    第62题 独一无二的路径走法

---
+++ 62_Unique_Paths.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1 for _ in range(m)] for _ in range(n)]
+
+        for i in range(1, n):
+            for j in range(1, m):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+
+        return dp[-1][-1]
+
+if __name__ == "__main__":
+    r = Solution().uniquePaths(23,12)
+    print r
