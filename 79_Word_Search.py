85dd14798b5b790f7010779327d988023668f90f
yoyocheknow <911dangerous@gmail.com>
2020-02-09 17:01:17 +0800

    79,查找word

--- 79_Word_Search.py
+++ 79_Word_Search.py
@@ -13,37 +13,36 @@ class Solution(object):
         if board == [[]]:
             return False
 
-        self.used = {}
+        used = {}
         self.i = 0
+        def check(i, j):
 
-        def check(i, y):
-            if board[y][x] != word[self.i]:
+            if board[i][j] != word[self.i]:
                 return False
 
             # make sure we have not used before
-            key = (x, y)
-            if key in self.used:
+            key = (i, j)
+            if key in used:
                 return False
-            self.used[key] = 1
-            self.i += 1
-
+            used[key] = 1
+            self.i+=1
             # if length matches we are done!
             if self.i == len(word):
                 return True
-            # try going right
-            if x + 1 < len(board[0]) and check(x + 1, y):
-                return True
             # try going down
-            if y + 1 < len(board) and check(x, y + 1):
+            if i + 1 < row and check(i + 1, j):
                 return True
-                # try going up
-            if y - 1 >= 0 and check(x, y - 1):
+            # try going right
+            if j + 1 < col and check(i, j + 1):
+                return True
+            # try going up
+            if i - 1 >= 0 and check(i-1, j):
                 return True
             # try going left
-            if x - 1 >= 0 and check(x - 1, y):
+            if j- 1 >= 0 and check(i, j-1):
                 return True
             self.i -= 1
-            del self.used[key]  # step back
+            del used[key]  # step back
 
             return False
 
